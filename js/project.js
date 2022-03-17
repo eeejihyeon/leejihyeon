1a3c971ef618a2acb252b070d37574913aee1d94
eeejihyeon <eeejihyeon@naver.com>
2022-03-17 20:14:27 +0900

    Project JS and CSS modified

---
+++ js/project.js
@@ -0,0 +1,32 @@
+const container = document.querySelector(".fade");
+const development = document.querySelector("#development");
+const design = document.querySelector("#design");
+const btn = document.querySelector(".sub");
+
+const HIDDEN_CLASS = "hidden";
+const FADEIN_CLASS = "fadein";
+const FADEOUT_CLASS = "fadeout";
+
+function changeProject() {
+  if (design.classList.contains(HIDDEN_CLASS)) {
+    btn.innerText = "Design";
+    development.classList.add(HIDDEN_CLASS);
+    design.classList.remove(HIDDEN_CLASS);
+
+    // // fadein & fadeout //
+    // development.classList.remove(FADEIN_CLASS);
+    // development.classList.add(FADEOUT_CLASS);
+    // design.classList.add(FADEIN_CLASS);
+  } else {
+    btn.innerText = "Front-end";
+    development.classList.remove(HIDDEN_CLASS);
+    design.classList.add(HIDDEN_CLASS);
+
+    // // fadein & fadeout //
+    // design.classList.remove(FADEIN_CLASS);
+    // design.classList.add(FADEOUT_CLASS);
+    // development.classList.add(FADEIN_CLASS);
+  }
+}
+
+btn.addEventListener("click", changeProject);
