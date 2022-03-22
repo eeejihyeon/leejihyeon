6ec878780ec7e6dc88e0030c0507849becee67a3
eeejihyeon <eeejihyeon@naver.com>
2022-03-22 13:30:04 +0900

    Night & Day mode change update

--- js/mode.js
+++ js/mode.js
@@ -1,37 +1,92 @@
 const body = document.body;
+const header = body.querySelector("header");
+const main = body.querySelector("main");
+const footer = body.querySelector("footer");
+
 const modeBtn = document.querySelector(".btn");
 const gnb = document.querySelector(".gnb");
+const introImgDay = document.querySelector("#introDay");
+const introImgNight = document.querySelector("#introNight");
+const introName = document.querySelector(".intro_name");
 const title = document.querySelectorAll(".title");
 const anchors = document.querySelectorAll("a");
+const paras = document.querySelectorAll("p");
+const divTexts = document.querySelectorAll("div");
+const questions = document.querySelectorAll(".faq_btn");
+const hrs = document.querySelectorAll(".line, .st_line, .foo_line");
 
-const DARK_COLOR = "#121213";
-const LIGHT_COLOR = "#f4f4f4";
+const NIGHT_BG = "night_bg";
+const NIGHT_A = "night_a";
+const NIGHT_TEXT = "night_text";
+const NIGHT_POINT_TEXT = "night_point_text";
+const NIGHT_BTN = "night_btn";
+
+function introChange() {
+  // intro image //
+  if (introImgNight.classList.contains(HIDDEN_CLASS)) {
+    introImgNight.classList.remove(HIDDEN_CLASS);
+    introImgDay.classList.add(HIDDEN_CLASS);
+  } else {
+    introImgDay.classList.remove(HIDDEN_CLASS);
+    introImgNight.classList.add(HIDDEN_CLASS);
+  }
+
+  // intro text //
+  introName.classList.toggle(NIGHT_POINT_TEXT);
+}
+
+function pointText() {
+  const langs = document.querySelectorAll(".lang");
+  const stNames = document.querySelectorAll(".st_name");
+  const subs = document.querySelectorAll(".sub");
+
+  langs.forEach((lang) => {
+    lang.classList.toggle(NIGHT_POINT_TEXT);
+  });
+
+  stNames.forEach((name) => {
+    name.classList.toggle(NIGHT_POINT_TEXT);
+  });
+
+  // change button //
+  subs.forEach((sub) => {
+    sub.classList.toggle(NIGHT_BTN);
+  });
+}
 
 function handleDayNightMode() {
   // body & gnb background color //
-  body.style.backgroundColor = DARK_COLOR;
-  gnb.style.backgroundColor = DARK_COLOR;
+  body.classList.toggle(NIGHT_BG);
+  gnb.classList.toggle(NIGHT_BG);
 
   // mode button //
-  modeBtn.innerText = "Day";
+  if ((modeBtn.innerText = "Night")) {
+    modeBtn.innerText = "Day";
+  } else {
+    modeBtn.innerText = "Night";
+  } // !!!!!!!!!! Day - Night 변경 왜 안 되지...
+  modeBtn.classList.toggle(NIGHT_BTN);
 
-  // anchor color //
+  // text color //
   anchors.forEach((anchor) => {
-    anchor.classList.toggle("night_a");
+    anchor.classList.toggle(NIGHT_A);
   });
 
-  // const textP = document.querySelectorAll("p");
-  // const textDiv = document.querySelectorAll("div");
-  // const textSpan = document.querySelectorAll(".st_det span");
+  divTexts.forEach((divText) => {
+    divText.classList.toggle(NIGHT_TEXT);
+  });
 
-  // if (modeBtn.innerText === "Day") {
-  //   gnbA.classList.add("a_night");
-  // }
+  questions.forEach((question) => {
+    question.classList.toggle("night_faq");
+  });
 
-  // for (i = 0; i <= 30; ++i) {
-  //   title[i].style.color = LIGHT_COLOR;
+  // hr //
+  hrs.forEach((hr) => {
+    hr.classList.toggle("night_hr");
+  });
 
-  // }
+  introChange();
+  pointText();
 }
 
 modeBtn.addEventListener("click", handleDayNightMode);
