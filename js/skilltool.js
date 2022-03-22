6b19dd89a748cae405c6f397a5c6163882900b67
eeejihyeon <eeejihyeon@naver.com>
2022-03-22 13:28:04 +0900

    CLASS_NAME delete

--- js/skilltool.js
+++ js/skilltool.js
@@ -2,19 +2,15 @@ const stDevelopment = document.querySelector("#st_development");
 const stDesign = document.querySelector("#st_design");
 const stBtn = document.querySelector(".st_wrap .sub");
 
-const HIDDEN_CLASSNAME = "hidden";
-const FADEIN_CLASSNAME = "fadein";
-const FADEOUT_CLASSNAME = "fadeout";
-
 function changeSkillTool() {
-  if (stDesign.classList.contains(HIDDEN_CLASSNAME)) {
+  if (stDesign.classList.contains(HIDDEN_CLASS)) {
     stBtn.innerText = "Design";
-    stDevelopment.classList.add(HIDDEN_CLASSNAME);
-    stDesign.classList.remove(HIDDEN_CLASSNAME);
+    stDevelopment.classList.add(HIDDEN_CLASS);
+    stDesign.classList.remove(HIDDEN_CLASS);
   } else {
     stBtn.innerText = "Front-end";
-    stDevelopment.classList.remove(HIDDEN_CLASSNAME);
-    stDesign.classList.add(HIDDEN_CLASSNAME);
+    stDevelopment.classList.remove(HIDDEN_CLASS);
+    stDesign.classList.add(HIDDEN_CLASS);
   }
 }
 
